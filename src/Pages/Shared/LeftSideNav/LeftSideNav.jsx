c85d4558ea8f6d61cac9985d955caf0ce1c01f42
unknown <you@example.com>
2024-06-22 01:23:44 +0600

    update

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -13,9 +13,17 @@ const LeftSideNav = () => {
         <div>
             <h2 className="text-xl">All Categoried</h2>
 
-            {
-                categories.map(category => <Link to={`/category/${category.id}`} className='block hover:underline p-2' key={category.id}>{category.name}</Link>)
-            }
+            <div className="overflow-x-auto whitespace-nowrap">
+                {
+                    categories.map(category => (
+                        <Link to={`/category/${category.id}`} className="inline-block md:block bg-gray-100 p-2" key={category.id}>
+                            <div className='bg-gray-700 text-white p-1 rounded  hover:bg-gray-900'>
+                                {category.name}
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
         </div>
     );
 };
